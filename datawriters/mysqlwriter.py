dd46cf90d07b115ea3f66e7ff5beea27b2469d43
James Marlowe <james.marlowe@lumate.com>
2015-07-22 00:19:14 -0500

    add data override option and password use

--- datawriters/mysqlwriter.py
+++ datawriters/mysqlwriter.py
@@ -12,6 +12,11 @@ class MysqlWriter:
         else:
             print 'missing user argument, using root'
             self.user_mysql = 'root'
+        if 'pass' in kwargs:
+            self.pass_mysql = kwargs['pass']
+        else:
+            print 'missing pass argument, skipping'
+            self.pass_mysql = ''
         if 'table' in kwargs:
             self.db_table = kwargs['table']
         else:
@@ -25,7 +30,11 @@ class MysqlWriter:
             return len(str(val))
         max_length = str(len(max(all_vals, key=key_order)))
         
-        db = mysql.connector.connect(user=self.user_mysql)
+        config = {user=self.user_mysql}
+        if self.pass_mysql != '':
+            config['password'] = self.pass_mysql
+        
+        db = mysql.connector.connect(**config)
         cursor = db.cursor()
         
         try:
@@ -64,11 +73,32 @@ class MysqlWriter:
         else:
             print("table already exists")
             
-        if 'ignore_duplicate_row_from_key' not in kwargs:
+        if 'expire_minutes' in kwargs:
+            expire_entries = ("CREATE EVENT IF NOT EXISTS Expire"+self.db_mysql+self.db_table+" "
+                              "ON SCHEDULE EVERY 5 MINUTE "
+                              "DO DELETE FROM "+self.db_table+" "
+                              "WHERE TIMESTAMPDIFF(MINUTE,"+self.db_table+".update_date, NOW())>"
+                              ""+str(kwargs['expire_minutes']))
+            cursor.execute(expire_entries)
+            
+        if 'override_previous_from_key' in kwargs:
+            existing_rows_from_key = ("select distinct("+kwargs['override_previous_from_key']+") from "+self.db_table)
+            cursor.execute(existing_rows_from_key)
+            existing_rows = set([item for (item,) in cursor])
+            #print "existing_rows", existing_rows
+            new_rows = set([d.get(kwargs['override_previous_from_key'], "NULL") for k in all_keys for d in list_of_dicts])
+            #print "new_rows", new_rows
+            overlap = list(set.intersection(existing_rows, new_rows))
+            #print "overlap", overlap
+            delete_entries = ("DELETE FROM "+self.db_table+" "
+                              "WHERE "+kwargs['override_previous_from_key']+" = %s")
+            #print delete_entries
+            #print [[o] for o in overlap]
+            cursor.executemany(delete_entries, [[o] for o in overlap])
             cursor.executemany("INSERT INTO "+self.db_table+" (" + ",".join(all_keys) + ") " +
                                "VALUES(" + ",".join(["%s"] * len(all_keys)) + ")",
                                [tuple(d.get(k, "NULL") for k in all_keys) for d in list_of_dicts])
-        else:
+        elif 'ignore_duplicate_row_from_key' in kwargs:
             existing_rows_from_key = ("select distinct("+kwargs['ignore_duplicate_row_from_key']+") from "+self.db_table)
             cursor.execute(existing_rows_from_key)
             existing_rows = [item for (item,) in cursor]
@@ -82,5 +112,9 @@ class MysqlWriter:
                                "VALUES(" + ",".join(["%s"] * len(all_keys)) + ")",
                                insert_vals)
             print "saving", len(insert_vals), "rows of",len(list_of_dicts) 
+        else:
+            cursor.executemany("INSERT INTO "+self.db_table+" (" + ",".join(all_keys) + ") " +
+                               "VALUES(" + ",".join(["%s"] * len(all_keys)) + ")",
+                               [tuple(d.get(k, "NULL") for k in all_keys) for d in list_of_dicts])
         db.commit()
 
